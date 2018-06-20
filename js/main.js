bdd213804c5464e83b7a74c594927037826daa27
Adi Gonnen <adigonnen@gmail.com>
2018-06-20 19:14:16 +0300

    add search mode

--- js/main.js
+++ js/main.js
@@ -3,16 +3,47 @@
 console.log('test');
 
 
+function init() {
+    renderGallery();
+}
 
-renderGallery(gImgs);
-
-function renderGallery(imgs) {
+function renderGallery() {
+    var imgs = getImgsForDisplay()
     var strHtml = '';
     imgs.forEach(function (img, idx) {
         strHtml += `<img id="${img.id}" class="item-img" onclick="selectImg(this)" style="background-image: url('../${img.url}')"></img>\n`
     });
     document.querySelector('.gallery').innerHTML = strHtml;
     // console.log(strHtml);
-    
-    
+}
+
+function searchImg() {
+   renderGallery();
+}
+
+function filterImgs(imgs) {
+    var userSearch = document.getElementById("search").value;
+    if (userSearch === '') return imgs;
+    else return imgs.filter(function(img) {
+        return img.keywords.forEach(function (keyword) {
+            return keyword === userSearch;
+            })
+        })
+}
+
+function getImgsForDisplay() {
+    var imgs = [];
+    imgs = filterImgs(gImgs)
+    return imgs;
+}
+
+function selectImg(elImg) {
+    toggleGallery();
+}
+
+function toggleGallery() {
+    var showCanvas = document.querySelector('.canvas');
+    showCanvas.classList.remove('hide');
+    var hideGallery = document.querySelector('.gallery');
+    hideGallery.classList.add('hide');
 }
