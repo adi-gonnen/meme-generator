cb23216534bc012f3a1884e9d8838d176380f3af
Adi Gonnen <adigonnen@gmail.com>
2018-06-21 12:32:16 +0300

    pull again

--- js/main.js
+++ js/main.js
@@ -18,17 +18,17 @@ function renderGallery(imgs) {
 }
 
 function searchImg() {
-    renderGallery();
+   renderGallery();
 }
 
 function filterImgs(imgs) {
     var userSearch = document.getElementById("search").value;
     if (userSearch === '') return imgs;
-    else return imgs.filter(function (img) {
+    else return imgs.filter(function(img) {
         return img.keywords.some(function (keyword) {
             return keyword.substring(0, userSearch.length) === userSearch;
+            });
         });
-    });
 }
 
 function getImgsForDisplay() {
@@ -41,21 +41,19 @@ function getImgsForDisplay() {
 
 function toggleGallery() {
     var elCanvas = document.querySelector('.container-canvas-page');
-    elCanvas.classList.toggle('hide');
+    elCanvas.classList.remove('hide');
     var elGallery = document.querySelector('.gallery');
-    elGallery.classList.toggle('hide');
+    elGallery.classList.add('hide');
     var elSearch = document.querySelector('.filter');
-    elSearch.classList.toggle('hide');
+    elSearch.classList.add('hide');
 }
+
 
 
 
 
 // CANVAS
 
-function backToGallery() {
-    toggleGallery();
-}
 
 function initCanvas(img) {
     gCanvas = document.querySelector('.canvas');
@@ -77,80 +75,15 @@ function onTxtInsert() {
 
 //render txt in canvas
 function renderTxtCanvas(txt) {
-
-}
-
-
-//get DOM txt
-function getTxtElement() {
-    //TODO: change the selector to txt on canvas
-    //TODO: RENDER CANVAS AND CLEAR
-    var elTextLabel = document.querySelector('.textlabel');
-    return elTextLabel;
-}
-
-function updateFontSizeOnEl(elTextLabel, updatedFontSize) {
-    // console.log('updatedFontSize-new !!-', updatedFontSize);
-    elTextLabel.style.fontSize = updatedFontSize + 'px';
-}
-
-//get current font size
-function getCurrFontSize(elTextLabel) {
-    var fontSizeTxt = window.getComputedStyle(elTextLabel, null).getPropertyValue('font-size');
-    var fontSize = parseFloat(fontSizeTxt);
-    return fontSize;
-}
-
-function updateFontSizeValue(elTextLabel, currFontSize, diff) {
-    var fontSizeTxt = elTextLabel.style.fontSize;
-    fontSizeTxt = (currFontSize + diff);
-    var updatedFontSize = parseFloat(fontSizeTxt);
-    return updatedFontSize;
-}
-
-function onIncreaseSize() {
-    onUpdateFontSize(1);
-}
-
-function onDecreaseSize() {
-    onUpdateFontSize(-1);
-}
-
-function onUpdateFontSize(diff) {
-    var elTextLabel = getTxtElement();
-    var currFontSize = getCurrFontSize(elTextLabel);
-    var updatedFontSize = updateFontSizeValue(elTextLabel, currFontSize, diff);
-    // console.log('currFontSize-??--', updatedFontSize);
-    updateFontSizeOnEl(elTextLabel, updatedFontSize);
-}
-
-//get color
-function getColorValue(colorValue) {
-    console.log('colorValue-??--', colorValue);
-    return changeTxtColor(colorValue);
-}
-
-//change color
-function changeTxtColor(colorValue) {
-    //TODO: change element - to txt element on canvas
-    var elTxt = document.querySelector('.txt-container .textlabel');
-    console.log('elTxt--', elTxt);
-    elTxt.style.color = colorValue;
-}
-
-//render txt in canvas
-function renderTxtCanvas(txt) {
     var canvas = document.getElementById('canvas');
-
-    // Make sure canvas is supported
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        ctx.font = "40px Impact";
-        //  ctx.color="white";
-        ctx.fillStyle = 'white';
-        ctx.fillText(txt, 248, 43);
-        ctx.save();
-    }
+ 
+  // Make sure canvas is supported
+  if (canvas.getContext){
+     var ctx = canvas.getContext('2d');
+     ctx.font = "40px Impact";       
+    //  ctx.color="white";
+    ctx.fillStyle = 'white';
+     ctx.fillText(txt, 248, 43);
+     ctx.save();      
+  }                  
 }
-
-
